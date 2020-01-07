9b96d1cfbd0cade673fea012d2c53115f5da6730
Carly <57219902+carlijnmaria@users.noreply.github.com>
2020-01-07 14:44:50 +0100

    Opdracht 6.
    Tweede uitwerking van opdracht 6.

--- Opdracht 6/index.php
+++ Opdracht 6/index.php
@@ -4,22 +4,17 @@
 $dbc = mysqli_connect('localhost', 'root', 'root', 'apen') or die ('Error connecting.');
 
 //QUERY SCHRIJVEN
-$query = "SELECT * FROM leefgebied";
-$query2 = "SELECT * FROM aap";
+$query = "SELECT * FROM aap JOIN aap_has_leefgebied ON aap.idaap = aap_has_leefgebied.idaap 
+JOIN leefgebied ON leefgebied.idleefgebied = aap_has_leefgebied.idleefgebied";
+
 
 //QUERY UITVOEREN
 $result = mysqli_query($dbc, $query) or die ('Error querying.');
-$result2 = mysqli_query($dbc, $query2) or die ('Error querying.');
 
 
 //REGEL VOOR REGEL IN BEELD BRENGEN
 while ($row = mysqli_fetch_array($result)){
-    echo $row['omschrijving'] . '<br>';
+    echo $row['omschrijving'] . ': ' . $row['soort'] . '<br>';
 }
 
-echo '<br>';
-
-while ($row = mysqli_fetch_array($result2)){
-    echo $row['soort'] . '<br>';
-}
 
